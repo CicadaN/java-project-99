915189eda638c0008c832c8620ef4e820476e745
cicad <cicadac@ya.ru>
2024-11-09 00:53:12 +0300

    fix bugs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,30 @@
 plugins {
 	java
+	id("application")
+	id("checkstyle")
+	id("jacoco")
 	id("org.springframework.boot") version "3.2.3"
 	id("io.spring.dependency-management") version "1.1.6"
+	id("io.freefair.lombok") version "8.4"
+	id("com.github.ben-manes.versions") version "0.48.0"
 }
 
 group = "hexlet.code"
 version = "0.0.1-SNAPSHOT"
 
+application {
+	mainClass.set("hexlet.code.AppApplication")
+}
+
 java {
 	toolchain {
 		languageVersion.set(JavaLanguageVersion.of(21))
 	}
+}
+
+checkstyle {
+	configFile = file("config/checkstyle/checkstyle.xml")
+	toolVersion = "10.13.0"
 }
 
 configurations {
