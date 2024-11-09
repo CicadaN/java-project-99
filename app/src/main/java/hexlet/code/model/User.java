fa8e503fe3ff49256292a162861f9d5ae8befb13
cicad <cicadac@ya.ru>
2024-11-09 12:50:44 +0300

    add app.yml, and User model

---
+++ app/src/main/java/hexlet/code/model/User.java
@@ -0,0 +1,31 @@
+package hexlet.code.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+}
